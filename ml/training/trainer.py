43a578c72e130522047e7b129257d0bfbc9addac
Sakeeb91 <rahman.sakeeb@gmail.com>
2026-02-16 23:44:46 -0500

    Implement core train_step with Monte Carlo supervision

--- ml/training/trainer.py
+++ ml/training/trainer.py
@@ -75,6 +75,46 @@ class FeynmanKacTrainer:
         self.history = TrainerHistory()
         self.global_step = 0
 
+    def _sample_training_batch(self, batch_size: int, n_mc_paths: int) -> tuple[torch.Tensor, torch.Tensor]:
+        x = self.problem.domain.sample_interior(batch_size, device=self.device)
+        targets, _ = feynman_kac_estimate(
+            x=x,
+            boundary_fn=self.problem.boundary_condition,
+            domain=self.problem.domain,
+            potential_fn=self.problem.potential,
+            n_paths=n_mc_paths,
+            device=self.device,
+        )
+        return x, targets
+
+    def _compute_grad_norm(self) -> float:
+        norm = torch.zeros((), device=self.device)
+        for parameter in self.model.parameters():
+            if parameter.grad is None:
+                continue
+            norm = norm + parameter.grad.detach().pow(2).sum()
+        return torch.sqrt(norm).item()
+
+    def train_step(self, batch_size: int, n_mc_paths: int) -> TrainStepMetrics:
+        """Run one optimization step against fresh MC supervision."""
+        self.model.train()
+        x, targets = self._sample_training_batch(batch_size=batch_size, n_mc_paths=n_mc_paths)
+        predictions = self.model(x).squeeze(-1)
+        loss = mc_mse_loss(predictions, targets)
+
+        self.optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        self.optimizer.step()
+        self.global_step += 1
+
+        grad_norm = self._compute_grad_norm()
+        current_lr = self.optimizer.param_groups[0]["lr"]
+        metrics = TrainStepMetrics(loss=loss.item(), lr=current_lr, grad_norm=grad_norm)
+        self.history.train_loss.append(metrics.loss)
+        self.history.lr.append(metrics.lr)
+        self.history.grad_norm.append(metrics.grad_norm)
+        return metrics
+
 
 __all__ = [
     "FKProblem",
