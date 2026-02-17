68d37d1073e414d2798f40baa8ba69a5166eaaa9
Sakeeb91 <rahman.sakeeb@gmail.com>
2026-02-16 23:30:03 -0500

    Export new Phase 1 domain and boundary APIs

--- ml/data/__init__.py
+++ ml/data/__init__.py
@@ -7,13 +7,50 @@ Contains:
 - Boundary condition handlers
 """
 
-from .brownian import simulate_brownian_paths, get_device
-from .domains import Hypercube, Hypersphere, Domain
+from .brownian import (
+    simulate_brownian_paths,
+    simulate_brownian_paths_batched,
+    feynman_kac_estimate,
+    get_device,
+)
+from .domains import (
+    Domain,
+    Hypercube,
+    Hypersphere,
+    HyperEllipsoid,
+    Interval,
+    Union,
+    Intersection,
+    visualize_domain_samples,
+)
+from .boundary import (
+    BoundaryCondition,
+    FluxBoundaryCondition,
+    DirichletBC,
+    NeumannBC,
+    sample_boundary_points,
+    sample_boundary_values,
+    estimate_outward_normals,
+)
 
 __all__ = [
     "simulate_brownian_paths",
+    "simulate_brownian_paths_batched",
+    "feynman_kac_estimate",
     "get_device",
     "Hypercube",
     "Hypersphere",
+    "HyperEllipsoid",
+    "Interval",
+    "Union",
+    "Intersection",
+    "visualize_domain_samples",
+    "BoundaryCondition",
+    "FluxBoundaryCondition",
+    "DirichletBC",
+    "NeumannBC",
+    "sample_boundary_points",
+    "sample_boundary_values",
+    "estimate_outward_normals",
     "Domain",
 ]
