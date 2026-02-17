d65b529b43e5a9a4579985f475f2f61e440ecc4e
Sakeeb91 <rahman.sakeeb@gmail.com>
2026-02-16 23:51:58 -0500

    Export Black-Scholes and Schrodinger problem classes

--- ml/problems/__init__.py
+++ ml/problems/__init__.py
@@ -1,5 +1,13 @@
 """Problem definitions for Feynman-Kac PINN experiments."""
 
 from .base import Problem
+from .black_scholes import BlackScholesND, BlackScholesParams
+from .schrodinger import HarmonicOscillatorND, HarmonicOscillatorParams
 
-__all__ = ["Problem"]
+__all__ = [
+    "Problem",
+    "BlackScholesND",
+    "BlackScholesParams",
+    "HarmonicOscillatorND",
+    "HarmonicOscillatorParams",
+]
