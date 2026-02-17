9fa48bcf88523b14b5e20185a5975d278621d039
Sakeeb91 <rahman.sakeeb@gmail.com>
2026-02-16 23:46:20 -0500

    Export Phase 2 training APIs

--- ml/training/__init__.py
+++ ml/training/__init__.py
@@ -1,1 +1,29 @@
 """Training utilities for Feynman-Kac PINN."""
+
+from .losses import l2_parameter_penalty, mc_huber_loss, mc_mse_loss, weighted_mc_mse_loss
+from .schedulers import (
+    SchedulerName,
+    WarmupConfig,
+    build_cosine_scheduler,
+    build_linear_warmup_scheduler,
+    build_plateau_scheduler,
+    build_scheduler,
+)
+from .trainer import FKProblem, FeynmanKacTrainer, TrainStepMetrics, TrainerHistory
+
+__all__ = [
+    "FKProblem",
+    "FeynmanKacTrainer",
+    "TrainStepMetrics",
+    "TrainerHistory",
+    "SchedulerName",
+    "WarmupConfig",
+    "build_scheduler",
+    "build_cosine_scheduler",
+    "build_plateau_scheduler",
+    "build_linear_warmup_scheduler",
+    "mc_mse_loss",
+    "weighted_mc_mse_loss",
+    "mc_huber_loss",
+    "l2_parameter_penalty",
+]
