8b3dabd161f82c6f725277b34c6c562bb39765ef
Sakeeb91 <rahman.sakeeb@gmail.com>
2026-02-16 23:52:33 -0500

    Export problem registry APIs from top-level ml package

--- ml/__init__.py
+++ ml/__init__.py
@@ -7,3 +7,25 @@ Feynman-Kac formula.
 """
 
 __version__ = "0.1.0"
+
+from .problems import (
+    BlackScholesND,
+    HarmonicOscillatorND,
+    Problem,
+    available_problems,
+    create_problem,
+    default_problem_configs,
+    deserialize_problem,
+    serialize_problem,
+)
+
+__all__ = [
+    "Problem",
+    "BlackScholesND",
+    "HarmonicOscillatorND",
+    "available_problems",
+    "default_problem_configs",
+    "create_problem",
+    "serialize_problem",
+    "deserialize_problem",
+]
